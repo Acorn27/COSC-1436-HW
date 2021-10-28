2935aeb763541dfaf3b3b03f158da201cbf863b2
Crab <mai.tran281@my.tccd.edu>
2021-10-27 23:54:32 -0500

    grade commentary program

---
+++ gradeCommentary.cpp
@@ -0,0 +1,42 @@
+/*
+Tarrant County College
+COSC-1436
+Professor Keith Smelser
+Mai Tran
+Asignment 3 week 11
+*/
+
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void displayComment(int);
+
+int main() {
+	// varibale to hold grade
+	int grade;
+
+	// prompt for user input
+	cout << "Enter your grade: ";
+	cin >> grade;
+	
+	//display commentary
+	displayComment(grade);
+	
+	// notify and terminate program
+	cout << "DOne.\n";
+	return 0;
+}
+
+void displayComment(int grade) {
+	if (grade < -10 || grade > 110) {
+		cout << "Invalid Grade.\n";
+		return;
+	} else if (grade >= 90) {
+		cout << "Good Job!\n";
+	} else if (grade >= 70) {
+		cout << "You may need to visit Lab\n";
+	} else {
+		cout << "Done\n";
+	}
+}
